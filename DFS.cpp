62ab1c21492acb25864bf09d05e9a77639c6ff90
Abdullah-Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2024-09-30 01:08:21 +0600

    DFS.cpp

---
+++ DFS.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+void DFS(int start, vector<vector<int>>& adj_list){
+    stack<int> Vnodes;
+    vector<bool> visited(adj_list.size(), false);
+    Vnodes.push(start);
+    visited[start] = true;
+    while(!Vnodes.empty()){
+        int current = Vnodes.top();
+        cout<<current<<" ";
+        Vnodes.pop();
+        for(int neighbour: adj_list[current]){
+            if(!visited[neighbour]){
+                visited[neighbour] = true;
+                Vnodes.push(neighbour);
+            }
+        }
+    }
+}
+int main() {
+    
+    vector<vector<int>> graph = {
+        {1, 2},    
+        {0, 3, 4}, 
+        {0},       
+        {1},       
+        {1, 5},    
+        {4}        
+    };
+
+    
+    cout << "DFS Traversal: "; // DFS Traversal: 0 2 1 4 5 3 
+    DFS(0, graph);
+
+    return 0;
+} 
+
+/*
+Graph:
+       0
+      / \
+     1   2
+    / \
+   3   4
+        \
+         5
+
+0 to 2 --->Traverse
+2 to 0 --->Backtrack
+0 to 1 --->Traverse
+1 to 4 --->Traverse
+4 to 5 --->Traverse
+5 to 4 --->Backtrack
+4 to 1 --->Backtrack
+1 to 3 --->Traverse
+
+*/
