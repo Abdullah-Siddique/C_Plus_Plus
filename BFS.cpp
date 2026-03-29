df329e0ec11438c43b0ff8ed75b24985919f6646
Abdullah Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2026-03-29 17:51:49 +0600

    Refactor BFS for long long support and level tracking
    Refactored BFS implementation to handle long long integers and added level tracking for each node.

--- BFS.cpp
+++ BFS.cpp
@@ -1,70 +1,41 @@
-/*
-Graph:
-       0
-      / \
-     1   2
-    / \
-   3   4
-        \
-         5
-
-Adjacency List:
-0 -> 1 2
-1 -> 0 3 4
-2 -> 0
-3 -> 1
-4 -> 1 5
-5 -> 4
-
-0 to 1 --->Traverse     
-0 to 2 --->Traverse
-1 to 3 --->Traverse
-1 to 4 --->Traverse
-4 to 5 --->Traverse
-5 to 4 --->Backtrack
-4 to 1 --->Backtrack
-3 to 1 --->Backtrack
-2 to 0 --->Backtrack
-
-       
-*/
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-void BFS(int start, vector<vector<int>>& adj_list) {
-    queue<int> Qnodes;
-    vector<bool> visited(adj_list.size(), false);
-
-    Qnodes.push(start);
-    visited[start] = true;
-
-    while (!Qnodes.empty()) {
-        int current = Qnodes.front();
-        cout << current << " ";
-        Qnodes.pop();
-
-        for (int neighbour : adj_list[current]) {
-            if (!visited[neighbour]) {
-                visited[neighbour] = true;
-                Qnodes.push(neighbour);
+using ll = long long;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll n;
+    cin>>n;
+    vector<vector<ll>>adj(n+1);
+    ll u, v;
+    while(cin>>u>>v){
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    vector<ll>vis(n+1), res, level(n+1, -1);
+    queue<ll>q;
+    ll src = 1; 
+    vis[src] = 1;
+    level[src] = 1;
+    q.push(src);
+    while(!q.empty()){
+        ll node = q.front();
+        res.push_back(node);
+        q.pop();
+        for(auto ng: adj[node]){
+            if(!vis[ng]){
+                vis[ng] = 1;
+                q.push(ng);
+                level[ng] = level[node]+1;
             }
         }
     }
-}
-
-int main() {
-    vector<vector<int>> graph = {
-        {1, 2},    // Neighbors of vertex 0
-        {0, 3, 4}, // Neighbors of vertex 1
-        {0},       // Neighbors of vertex 2
-        {1},       // Neighbors of vertex 3
-        {1, 5},    // Neighbors of vertex 4
-        {4}        // Neighbors of vertex 5
-    };
-
-    cout << "BFS Traversal: "; // BFS Traversal: 0 1 2 3 4 5
-    BFS(0, graph);
-
+    cout<<"BFS: \n";
+    for(auto e: res)cout<<e<<" ";
+    cout<<"\n";
+    for(ll i=0; i<=n; i++){
+        cout<<"Level of node "<<i<<" is "<<level[i]<<" \n";
+    }
     return 0;
+    
 }
