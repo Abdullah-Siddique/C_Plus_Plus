84ddfae6af6c55ab1cf60d49f07fe51f6b87cb2d
Abdullah Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2026-07-27 22:14:01 +0600

    Refactor BFS implementation and improve output

--- BFS.cpp
+++ BFS.cpp
@@ -1,64 +1,119 @@
+
 /*
-My example:
-7
+Example input:
+8 8 1 4
 1 2
-1 5
+1 7
 2 3
-2 4
-5 6
-6 7
+2 5
+2 6
+3 4
+4 5
+7 8
 
 Output:
-BFS: 
-1 2 5 3 4 6 7 
-Level of node 0 is -1 
-Level of node 1 is 1 
-Level of node 2 is 2 
-Level of node 3 is 3 
-Level of node 4 is 3 
-Level of node 5 is 2 
-Level of node 6 is 3 
-Level of node 7 is 4 
 
+Adjacency List:
+
+2 7
+1 3 5 6
+2 4
+3 5
+2 4
+2
+1 8
+7
+BFS array:
+1 2 7 3 5 6 8 4
+Path from 1 to 4 :
+1 2 3 4
+Distance of 0 from 1 is -1
+Distance of 1 from 1 is 0
+Distance of 2 from 1 is 1
+Distance of 3 from 1 is 2
+Distance of 4 from 1 is 3
+Distance of 5 from 1 is 2
+Distance of 6 from 1 is 2
+Distance of 7 from 1 is 1
+Distance of 8 from 1 is 2
+
+Parent of node 0 is -1
+Parent of node 1 is -1
+Parent of node 2 is 1
+Parent of node 3 is 2
+Parent of node 4 is 3
+Parent of node 5 is 2
+Parent of node 6 is 2
+Parent of node 7 is 1
+Parent of node 8 is 7
 */
+
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    ll n;
-    cin>>n;
-    vector<vector<ll>>adj(n+1);
-    ll u, v;
-    while(cin>>u>>v){
+    ll n, e, src, dest;
+    cin >> n >> e >> src >> dest;
+    vector<vector<ll>> adj(n+1);
+    for(ll i = 0; i < e; i++){
+        ll u, v;
+        cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    vector<ll>vis(n+1), res, level(n+1, -1);
-    queue<ll>q;
-    ll src = 1; 
-    vis[src] = 1;
-    level[src] = 1;
+    cout << "\nAdjacency List: \n";
+    for(ll i = 0; i <= n; i++){
+        for(auto e : adj[i]) cout << e << ' ';
+        cout << '\n';
+    }
+    vector<bool> vis(n + 1, false);
+    queue<ll> q;
+    vector<ll> d(n + 1), p(n + 1), bfs;
+    d[0] = p[0] = -1;
+    vis[src] = true;
     q.push(src);
+    p[src] = -1;
     while(!q.empty()){
         ll node = q.front();
-        res.push_back(node);
         q.pop();
-        for(auto ng: adj[node]){
+        bfs.push_back(node);
+        for(auto ng : adj[node]){
             if(!vis[ng]){
-                vis[ng] = 1;
+                vis[ng] = true;
                 q.push(ng);
-                level[ng] = level[node]+1;
+                d[ng] = d[node] + 1;
+                p[ng] = node;
             }
         }
     }
-    cout<<"BFS: \n";
-    for(auto e: res)cout<<e<<" ";
-    cout<<"\n";
-    for(ll i=0; i<=n; i++){
-        cout<<"Level of node "<<i<<" is "<<level[i]<<" \n";
+    cout << "BFS array:\n";
+    for(auto e : bfs) cout << e << ' ';
+    cout << '\n';
+    if(!vis[dest]) cout << "NO PATH\n";
+    else{
+        vector<ll> path;
+        for(ll v = dest; v != -1; v = p[v]){
+            path.push_back(v);
+        }
+        reverse(path.begin(), path.end());
+        cout << "Path from " << src << " to " << dest << " : \n";
+        for(ll v : path) cout << v << ' ';
+        cout << '\n';
     }
+    for(ll i = 0; i <= n; i++){
+        cout << "Distance of " << i <<" from " << src << " is " << d[i];
+        cout << '\n';
+    }
+    cout << '\n';
+
+    for(ll i = 0; i <= n; i++){
+        cout << "Parent of node " << i <<" is " << p[i];
+        cout << '\n';
+    }
+    cout << '\n';
+
     return 0;
-    
 }
