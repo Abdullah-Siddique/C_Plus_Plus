66c9369ac6bec55bd152d0f3a9eaf3f33079d19d
Abdullah Siddique <137615484+Abdullah-Siddique@users.noreply.github.com>
2026-08-23 21:44:16 +0600

    Add Lucas theorem implementation in Lucas.cpp

---
+++ Lucas.cpp
@@ -0,0 +1,64 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll mod = 1000003;
+const ll N = 1000003;
+ll f[N], fi[N];
+
+ll pw(ll x, ll n){
+    x %= mod;
+    ll ans = 1;
+    while(n){
+        if(n & 1) ans = (ans * x) % mod;
+        x = (x * x) % mod;
+        n >>= 1;
+    }
+    return ans;
+}
+
+ll minv(ll n){
+    return pw(n, mod - 2);
+}
+
+void compute(){
+    f[0] = fi[0] = 1;
+    for(ll i = 1; i < N; i++) f[i] = (f[i - 1] * i) % mod;
+    fi[N - 1] = minv(f[N - 1]);
+    for(ll i = N - 2; i >= 1; i--){
+        fi[i] = (fi[i + 1] * (i + 1)) % mod;
+    }
+}
+
+ll ncr(ll n, ll r){
+    if(r < 0 || r > n) return 0;
+    ll ans = f[n];
+    ans = (ans * fi[r]) % mod;
+    ans = (ans * fi[n - r]) % mod;
+
+    return ans;
+}
+
+
+ll lucas(ll n, ll r){
+    if(r < 0 || r > n) return 0;
+    if(r == 0) return 1;
+    return (lucas(n / mod, r / mod) * ncr(n % mod, r % mod)) % mod;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    compute();
+    ll t;
+    cin >> t;
+    for(ll i = 1; i <= t; i++){
+        ll a, b;
+        cin >> a >> b;
+        //cout << "Case " << i << ": ";
+        cout << lucas(a, b) << '\n';
+    }
+
+    return 0;
+}
